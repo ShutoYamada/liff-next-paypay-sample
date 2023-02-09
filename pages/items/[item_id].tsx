9be41fbe41099e35152aa81e570457afbeabbac0
ShutoYamada <stym1122@gamil.com>
2023-02-09 14:29:22 +0900

    LIFFライブラリのSSR対応

--- pages/items/[item_id].tsx
+++ pages/items/[item_id].tsx
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import liff, { Liff } from "@line/liff";
 import { Center, Container } from "@mantine/core";
 import ItemDetailCard from "../../components/ItemDetailCard";
 import ITEM_LIST from "../../mocks/ITEM_LIST";
@@ -10,25 +9,28 @@ const RoomDetailPage = () => {
   // パスパラメータから値を取得
   const { item_id } = router.query;
   const item = ITEM_LIST.find((item) => item.id === item_id);
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffObject, setLiffObject] = useState<any | null>(null);
   const [status, setStatus] = useState<string>("LINEログイン中...");
   const [isLogined, setIsLogined] = useState<boolean>(false);
 
   useEffect(() => {
-    liff
-      .init({ liffId: process.env.LIFF_ID as string })
-      .then(() => {
-        setLiffObject(liff);
-        if (liff.isLoggedIn()) {
-          // ログインの確認を取れたら
-          setStatus("LINEログインに成功しました。");
-          setIsLogined(true);
-        }
-      })
-      .catch((err: any) => {
-        console.error({ err });
-        setStatus("LINEログインに失敗しました。");
-      });
+    // SSR対応のため動的importしている
+    import("@line/liff").then((liff: any) => {
+      liff
+        .init({ liffId: process.env.LIFF_ID as string })
+        .then(() => {
+          setLiffObject(liff);
+          if (liff.isLoggedIn()) {
+            // ログインの確認を取れたら
+            setStatus("LINEログインに成功しました。");
+            setIsLogined(true);
+          }
+        })
+        .catch((err: any) => {
+          console.error({ err });
+          setStatus("LINEログインに失敗しました。");
+        });
+    });
   }, []);
 
   const onClickPurchase = useCallback(() => {
