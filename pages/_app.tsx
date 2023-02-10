b280483486e35ed829381b0b9bafbc4f58a4e71c
ShutoYamada <stym1122@gamil.com>
2023-02-10 13:32:21 +0900

    LIFFログインの箇所を修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [liffObject, setLiffObject] = useState<any | null>(null);
+
+  useEffect(() => {
+    // SSR対応のため動的importしている
+    import("@line/liff").then((liff: any) => {
+      liff
+        .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID as string })
+        .then(() => {
+          setLiffObject(liff);
+          if (liff.isLoggedIn()) {
+            // ログインの確認を取れたら
+            // setStatus("LINEログインに成功しました。");
+            // setIsLogined(true);
+          }
+        })
+        .catch((err: any) => {
+          console.error({ err });
+          // setStatus("LINEログインに失敗しました。");
+        });
+    });
+  }, []);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
--- pages/items/[item_id].tsx
+++ pages/items/[item_id].tsx
@@ -9,27 +9,40 @@ const RoomDetailPage = () => {
   // パスパラメータから値を取得
   const { item_id } = router.query;
   const item = ITEM_LIST.find((item) => item.id === item_id);
-  const [liffObject, setLiffObject] = useState<any | null>(null);
+  //const [liffObject, setLiffObject] = useState<any | null>(null);
   const [status, setStatus] = useState<string>("LINEログイン中...");
   const [isLogined, setIsLogined] = useState<boolean>(false);
 
+  // useEffect(() => {
+  //   // SSR対応のため動的importしている
+  //   import("@line/liff").then((liff: any) => {
+  //     liff
+  //       .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID as string })
+  //       .then(() => {
+  //         setLiffObject(liff);
+  //         if (liff.isLoggedIn()) {
+  //           // ログインの確認を取れたら
+  //           setStatus("LINEログインに成功しました。");
+  //           setIsLogined(true);
+  //         }
+  //       })
+  //       .catch((err: any) => {
+  //         console.error({ err });
+  //         setStatus("LINEログインに失敗しました。");
+  //       });
+  //   });
+  // }, []);
+
   useEffect(() => {
-    // SSR対応のため動的importしている
     import("@line/liff").then((liff: any) => {
-      liff
-        .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID as string })
-        .then(() => {
-          setLiffObject(liff);
-          if (liff.isLoggedIn()) {
-            // ログインの確認を取れたら
-            setStatus("LINEログインに成功しました。");
-            setIsLogined(true);
-          }
-        })
-        .catch((err: any) => {
-          console.error({ err });
-          setStatus("LINEログインに失敗しました。");
-        });
+      if (liff.isLoggedIn()) {
+        const context = liff.getContext();
+        const liffToken = liff.getAccessToken();
+        // setUid(context.userId)
+        // setAccessToken(liffToken);
+        setIsLogined(true);
+        setStatus("LINEログインに成功しました。");
+      }
     });
   }, []);
 
